6708ff8b34b3fafe1665370abd4c410ba33a5d9a
JungYeon22 <jyp7558@gmail.com>
2024-02-28 11:46:52 +0900

    main setting

---
+++ math/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 14;
+        int b = 3;
+
+        // add(a, b) - 더하기
+
+        // sub(a, b) - 빼기
+
+        // div(a, b) - 나누기
+
+        // mul(a, b) - 곱하기
+    }
+}
